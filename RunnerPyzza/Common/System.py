de3d869bd0f1c6f687ece25f4d42340750789ee8
Emilio Potenza <aemilius1984@gmail.com>
2011-11-30 19:46:01 +0100

    Emilio Potenza 00002 Prova Funzionante di RPdaemon in coppia con test2.py Prima consegna effettuata!
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   RunnerPyzza/Common/System.py
    	deleted:    RunnerPyzza/RPdaemon
    	deleted:    RunnerPyzza/ServerCommon/Server.pyc
    	new file:   RunnerPyzza/__init__.py
    
     Changes not staged for commit:
       (use "git add <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   RunnerPyzza/Common/Machine.py
    	modified:   RunnerPyzza/Common/Program.py
    	modified:   RunnerPyzza/Common/PyzzaProtocol.py
    	modified:   RunnerPyzza/ServerCommon/Server.py
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	RPdaemon
    	RPdaemon.log
    	RPdaemon~
    	RunnerPyzza/Common/JSON.pyc
    	RunnerPyzza/Common/Machine.pyc
    	RunnerPyzza/Common/Machine.py~
    	RunnerPyzza/Common/Program.pyc
    	RunnerPyzza/Common/Program.py~
    	RunnerPyzza/Common/PyzzaProtocol.pyc
    	RunnerPyzza/Common/PyzzaProtocol.py~
    	RunnerPyzza/Common/System.pyc
    	RunnerPyzza/Common/System.py~
    	RunnerPyzza/Common/__init__.pyc
    	RunnerPyzza/ServerCommon/Server.pyc
    	RunnerPyzza/ServerCommon/Server.py~
    	RunnerPyzza/ServerCommon/__init__.pyc
    	RunnerPyzza/__init__.pyc

---
+++ RunnerPyzza/Common/System.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+Machine
+
+Common package
+
+Objects that represent a System message
+"""
+
+__author__ = "Emilio Potenza"
+__copyright__ = "Copyright 2011, RunnerPyzza"
+__credits__ = ["Marco Galardini"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Marco Galardini"
+__email__ = "emilio.potenza@iasma.it"
+__status__ = "Development"
+
+################################################################################
+# Imports
+
+import logging
+
+################################################################################
+# Log setup
+
+# create logger
+# Name shown
+logger = logging.getLogger('System')
+
+################################################################################
+# Classes
+
+class System(object):
+    '''
+    Class System
+    '''
+    def __init__(self,cmd):
+        self.cmd = cmd
+ 
+    def msg(self):
+        '''
+        Returns a dictionary representation of the object
+        To be sent to the server
+        '''
+        d = {}
+        d['type'] = 'system'
+        # Values dictionary
+        d1 = {}
+        d1['msg'] = self.cmd
+        # Put all together
+        d['values'] = d1
+
+        return d
+    
+
+################################################################################
+# Methods
+
+################################################################################
+# Main
+
+if __name__ == '__main__':
+    pass
