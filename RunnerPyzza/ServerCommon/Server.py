6d96f0170720499ddb7c350d42e1e83dfc8a4b58
Emilio Potenza <aemilius1984@gmail.com>
2011-12-05 23:28:00 +0100

    update

--- RunnerPyzza/ServerCommon/Server.py
+++ RunnerPyzza/ServerCommon/Server.py
@@ -17,7 +17,7 @@ __email__ = "emilio.potenza@iasma.it"
 __status__ = "Development"
 
 #################################################################################
-# Imports
+# Imports ##
 
 import logging
 import socket
@@ -25,7 +25,9 @@ from getpass import getpass
 import threading 	
 import Queue
 import paramiko
-from Common.PyzzaProtocol import *
+from RunnerPyzza.Common.JSON import JSON
+from RunnerPyzza.Common.System import System
+from RunnerPyzza.Common.PyzzaProtocol import PyzzaProtocol
 
 ################################################################################
 # Log setup
@@ -37,120 +39,189 @@ logger = logging.getLogger('Server')
 
 ################################################################################
 
+#Tread del manager code
+class WorkerJob(threading.Thread):
+	def __init__ (self,job):
+		threading.Thread.__init__(self)
+		self.name=job.name
+		self.thread_stop=False
+		self.programs=job.programs
+		###TEST CONVERSION###
+		self.queue=Queue.Queue()
+		for p in job.programs:
+			self.queue.put(p.getCmd())
+			
+		#####################
+		self.machines=job.machines
+		self.connections=[]
+		self.threads = []
+		self.outlock = threading.Lock()
+
+	def _connect(self):
+		"""Connect to all hosts in the hosts list"""
+		for host in self.machines:
+		    client = paramiko.SSHClient()
+		    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		    client.connect(host.getHostname(), username=host.getUser(), password=host.getPass())
+		    self.connections.append(client)
+
+
+	def _workFun(self,host, conn, queue):
+		while True:
+			try:
+				command=queue.get()
+				if command=="break":
+					queue.task_done()
+					break	
+				#self.raw_notify("%s> Command in queue start"%(host[0]),command)
+				with self.outlock:
+					print "... %s ==> %s"%(host.getHostname(),command)
+				stdin, stdout, stderr = conn.exec_command(command)
+
+				stdin.close()
+				for line in stdout.read().splitlines():
+				    with self.outlock:
+				    	print """\033[1;32m[%s - out]\033[0m : %s""" % (host.getHostname(), line) 
+				for line in stderr.read().splitlines():
+				    with self.outlock:
+				    	print """\033[1;31m[%s - err]\033[0m : %s""" % (host.getHostname(), line) 		
+				#self.raw_notify("%s> Command in queue done"%(host.getHostname()),command)
+				queue.task_done()
+			except KeyboardInterrupt:
+				print "do quit thread"
+				self.do_quit(args)
+				break
+			except Exception as e:
+				print e	
+
+	def _quit(self):
+		"""Close all the connections and exit"""
+		for conn in self.connections:
+		    conn.close()
+		return 
+
+	def run(self):
+		'''Connect'''
+		self._connect()
+		#print self.hosts
+		#print self.connections
+		#print self.queue
+		"""start Execute commands queue on all hosts in the list"""
+		try:		
+			for host, conn in zip(self.machines, self.connections):
+				self.queue.put("break")
+				t = threading.Thread(target=self._workFun, args=(host,conn, self.queue))
+				t.setDaemon(True)		
+				t.start()
+				self.threads.append(t)
+			self.queue.join()
+		except KeyboardInterrupt:
+			print "do quit"
+			self._quit()
+		self._quit()		
+
+class WorkerManager():
+	'''
+	Manager for multiple jobs
+	'''
+	def __init__(self):
+		self._jobs = {} # jobID:{"machine":[machinediz1, ..], "program":[programdiz1,...]}
+
+	def getJob(self,name):
+		return self._jobs[name]
+	def addJob(self,job):
+		self._jobs[job.name] = job
+		#START-JOB
+	def startJob(self,name):
+		self.test(name)
+	def test(self,name):
+		job=self._jobs[name]
+		print job.name
+		print job.machines
+		print job.programs
+		j=WorkerJob(job)
+		j.start()
+
+class Job():
+	'''
+	Job structure (and record??) running in WorkerManager
+	'''
+	def __init__(self,jobID):
+		self.name = jobID
+		self.machines = []
+		self.programs = []
+		
+		
+
 class Server():
     '''
     RunnerPyzza daemon server (RPdaemon)
     '''
     def __init__ (self,port):
-	#self.hosts = []
-	#self.queue = Queue.Queue()
-	self.jobsList = {} # jobID:{"machine":[machinediz1, ..], "program":[programdiz1,...]}
+	 
 	self.jobCounter = 0
-
+	self.manager = WorkerManager()
+	self.msgHandler = JSON()
+	####
+	# Create a server
 	host = ""
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	server_socket.bind((host, port))
 	server_socket.listen(5)
-	#logging.debug("Type 'Q' or 'q' to QUIT")
+	#
+	####
 	logging.debug("Server - Waiting for client on port %s"%(port))
-	#print "wait"
 	quit=False
 	while not quit:
 	    	#Attendi la connessione del lanciatore...
 	   	client_socket, address = server_socket.accept()
 	    	logging.debug("Server - Connection from %s %s"%(address))
 		####
-		#
 		# Create a new job
-		#
 		self.jobCounter+=1
-		self.jobsList[self.jobCounter]={"machine":[],"program":[]}
+		job=Job(self.jobCounter)
 		#
 		####
+		ok = System("ok")
+		ok = self.msgHandler.encode(ok.msg())
+		print ok
 		while 1:
 			logging.debug("...waiting ")
 		    	client_data = client_socket.recv(1024)
-		
 			###
-			#
-			# Inserire PyzzaProtocoll 
-			# TEST
+			# PyzzaProtocoll 
 			PP=PyzzaProtocol(client_data)
-			PPtype = PP.isType()
-			PPdict = PP.toDict()
-			#print PPtype
-			#client_socket.send('{"values": {"msg" : "ok"}, "type" : "system"}')
 			#
 			###		
-				
-			if PPtype=="system":
-				
-				if PPdict["values"]["msg"]=="quit":
+			if PP.type=="system":
+				if PP.obj.cmd == "quit":
 					logging.debug("\n Bye Bye !!\n")
 					client_socket.close()
 					break
 				else:
 					logging.debug("\n ?? \n%s"%(client_data))
 				
-			elif PPtype == "machine":	
+			elif PP.type == "machine":	
 				logging.debug("Machine :%s"%(client_data))
-				self.jobsList[self.jobCounter]["machine"].append()
-				client_socket.send('{"values": {"msg" : "ok"}, "type" : "system"}')
+				job.machines.append(PP.obj)
+				client_socket.send(ok)
 
-			elif PPtype == "program":	
+			elif PP.type == "program":	
 				logging.debug("Program :%s"%(client_data))
-				client_socket.send('{"values": {"msg" : "ok"}, "type" : "system"}')
+				job.programs.append(PP.obj)
+				client_socket.send(ok)
 
 			else:
 				logging.debug("FAIL :%s"%(client_data))
-				client_socket.send('{"values": {"msg" : "fail"}, "type" : "system"}')
-
-
+				client_socket.send(System("fail"))
+		####
+		# Append to the main queue and start the job
+		self.manager.addJob(job)
+		self.manager.startJob(job.name)
+		#
+		####
 					
-			
-			'''if client_data.lower() == 'quit_server':
-				logging.info("QUIT")
-				client_socket.close()
-				quit=True
-				break
-			elif client_data.lower() == 'q':
-				logging.info("Client disconnected")
-				client_socket.close()
-				break
-			elif client_data.find("cmd:")==0:
-				client_data=client_data.split(":")[1]
-				self.queue.put("%s"%(client_data))
-				#"cmd:<command>"
-				logging.info("add command to queue %s"%(client_data))
-				client_socket.send("ok")
-
-			elif client_data.find("server:")==0:
-				client_data=client_data.split(":")[1]
-				self.hosts.append(client_data.split(','))
-        			#server:hostip,user,password
-				logging.info("add server to cluster%s"%(client_data))
-				client_socket.send("ok")
-
-			elif client_data.find("start")==0:
-				queue_test=RPqueue(self.hosts,self.queue)
-				queue_test.start()
-				logging.info("Start queue")
-				client_socket.send("ok")
-				self.queue=Queue.Queue()
-
-			elif client_data.find("debug")==0:
-				print self.hosts,self.queue
-				client_socket.send("ok")
-			else:
-				#print "--X server: ", server_data, "fail"
-				#print "Allowed server commands: q, Q [Close client]; 
-				#cmd:xxx [Add command to queue]; server:xxx [Add server to cluster]" 
-				logging.info("fail")
-				client_socket.send("fail")
-				#print "fail"
-			'''
-
 ################################################################################
 # Methods
 
