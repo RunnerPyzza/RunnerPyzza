d947bc2d31640dfeccf3f82b43b037a5a675171a
Marco Galardini <galactus8403@gmail.com>
2012-02-21 18:35:21 +0100

    Indentation fix

--- RunnerPyzza/ServerCommon/Server.py
+++ RunnerPyzza/ServerCommon/Server.py
@@ -23,7 +23,7 @@ import logging
 import socket
 import sys
 from getpass import getpass
-import threading 	
+import threading     
 import Queue
 import paramiko
 import time
@@ -44,160 +44,160 @@ logger = logging.getLogger('Server')
 
 #Tread del manager code
 class WorkerJob(threading.Thread):
-	def __init__ (self,job):
-		threading.Thread.__init__(self)
-		self.name = job.name
-		self.thread_stop = False
-		self.programs = job.programs
-                self.listOFqueue = []
+    def __init__ (self,job):
+        threading.Thread.__init__(self)
+        self.name = job.name
+        self.thread_stop = False
+        self.programs = job.programs
+        self.listOFqueue = []
                 
-		###TEST CONVERSION###
-                #order from 0 to infinite
-                order = 0
-                while True:
-                    queue = Queue.Queue()
-                    for p in job.programs:
-                        if int(p.getOrder()) == order:
-                            queue.put(p.getCmd())
-                    if queue.empty():
-                        break
-                    else:
-                        self.listOFqueue.append(queue)
-                    order += 1
-                print self.listOFqueue
+        ###TEST CONVERSION###
+        #order from 0 to infinite
+        order = 0
+        while True:
+            queue = Queue.Queue()
+            for p in job.programs:
+                if int(p.getOrder()) == order:
+                    queue.put(p.getCmd())
+            if queue.empty():
+                break
+            else:
+                self.listOFqueue.append(queue)
+            order += 1
+        print self.listOFqueue
                     
-			
-		#####################
-                self.job=job
-		self.machines=job.machines
-		self.connections=[]
-		self.threads = []
-		self.outlock = threading.Lock()
+            
+        #####################
+        self.job=job
+        self.machines=job.machines
+        self.connections=[]
+        self.threads = []
+        self.outlock = threading.Lock()
 
-	def _connect(self):
-		"""Connect to all hosts in the hosts list"""
-		for host in self.machines:
-			client = paramiko.SSHClient()
-			client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                        print host.getHostname(), host.getUser(), host.getPassword()
-			client.connect(host.getHostname(), username=host.getUser(), password=host.getPassword())
-			self.connections.append(client)
-                        logging.info("Job %s: %s is now connected to user %s"%(self.name, host.getHostname(),host.getUser()))
+    def _connect(self):
+        """Connect to all hosts in the hosts list"""
+        for host in self.machines:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            #print host.getHostname(), host.getUser(), host.getPassword()
+            client.connect(host.getHostname(), username=host.getUser(), password=host.getPassword())
+            self.connections.append(client)
+            logging.info("Job %s: %s is now connected to user %s"%(self.name, host.getHostname(),host.getUser()))
 
 
-	def _workFun(self,host, conn, queue, step):
-		while True:
-			try:
-				command=queue.get()
-				if command=="break":
-					queue.task_done()
-					break	
-				#self.raw_notify("%s> Command in queue start"%(host[0]),command)
-				with self.outlock:
-					logging.info("... %s ==> %s"%(host.getHostname(),command))
+    def _workFun(self,host, conn, queue, step):
+        while True:
+            try:
+                command=queue.get()
+                if command=="break":
+                    queue.task_done()
+                    break    
+                #self.raw_notify("%s> Command in queue start"%(host[0]),command)
+                with self.outlock:
+                    logging.info("... %s ==> %s"%(host.getHostname(),command))
                                 
-                                chan = conn.get_transport().open_session()
-                                chan.exec_command(command)
-                                stdout = chan.makefile("rb", 1024)
-                                stderr = chan.makefile_stderr("rb", 1024)
-                                #stdin, stdout, stderr = conn.exec_command(command)
-				#stdin.close()
-                                for line in stdout.read().splitlines():
-					with self.outlock:
-						logging.info("""\033[1;32m[%s - out]\033[0m : %s""" % (host.getHostname(), line))
+                    chan = conn.get_transport().open_session()
+                    chan.exec_command(command)
+                    stdout = chan.makefile("rb", 1024)
+                    stderr = chan.makefile_stderr("rb", 1024)
+                    #stdin, stdout, stderr = conn.exec_command(command)
+                    #stdin.close()
+                    for line in stdout.read().splitlines():
+                        with self.outlock:
+                            logging.info("""\033[1;32m[%s - out]\033[0m : %s""" % (host.getHostname(), line))
                                                 #self.job.stdout=self.job.stdout+"""\033[1;32m[%s - out]\033[0m : %s\n""" % (host.getHostname(), line)
-                                                self.job.stdout.put("""\033[1;32m[%s - out]\033[0m : %s\n""" % (host.getHostname(), line))
-                                                print """\033[1;32m[%s - out]\033[0m : %s\n""" % (host.getHostname(), line)
-				for line in stderr.read().splitlines():
-					with self.outlock:
-                                                logging.info("""\033[1;31m[%s - err]\033[0m : %s""" % (host.getHostname(), line))
-						#self.job.stderr=self.job.stderr+"""\033[1;31m[%s - err]\033[0m : %s\n""" % (host.getHostname(), line)
-                                                self.job.stderr.put("""\033[1;31m[%s - err]\033[0m : %s\n""" % (host.getHostname(), line))
-                                                print """\033[1;31m[%s - err]\033[0m : %s\n""" % (host.getHostname(), line)
-                                exit_status = chan.exit_status
-                                chan.close() 
-				queue.task_done()
+                            self.job.stdout.put("""\033[1;32m[%s - out]\033[0m : %s\n""" % (host.getHostname(), line))
+                            print """\033[1;32m[%s - out]\033[0m : %s\n""" % (host.getHostname(), line)
+                for line in stderr.read().splitlines():
+                    with self.outlock:
+                        logging.info("""\033[1;31m[%s - err]\033[0m : %s""" % (host.getHostname(), line))
+                        #self.job.stderr=self.job.stderr+"""\033[1;31m[%s - err]\033[0m : %s\n""" % (host.getHostname(), line)
+                        self.job.stderr.put("""\033[1;31m[%s - err]\033[0m : %s\n""" % (host.getHostname(), line))
+                        print """\033[1;31m[%s - err]\033[0m : %s\n""" % (host.getHostname(), line)
+                        exit_status = chan.exit_status
+                        chan.close() 
+                queue.task_done()
                                 
-                                with self.outlock:
-                                    self.job.status.put("%s||"%step + command + "||%s"%exit_status)
-                                    print "%s||"%step + command + "||%s"%exit_status
-                                    if exit_status != 0:
-                                        self.job.status_error = True
-                                        self.job.error.put("%s||"%step + command + "||%s"%exit_status)
+                with self.outlock:
+                    self.job.status.put("%s||"%step + command + "||%s"%exit_status)
+                    print "%s||"%step + command + "||%s"%exit_status
+                    if exit_status != 0:
+                        self.job.status_error = True
+                        self.job.error.put("%s||"%step + command + "||%s"%exit_status)
                                 
-			except KeyboardInterrupt:
-				print "do quit thread"
-				self._quit(None)
-				break
-			except Exception as e:
-				print e	
+            except KeyboardInterrupt:
+                print "do quit thread"
+                self._quit(None)
+                break
+            except Exception as e:
+                print e    
 
-	def _quit(self):
-		"""Close all the connections and exit"""
-		for conn in self.connections:
-			conn.close()
-		return 
+    def _quit(self):
+        """Close all the connections and exit"""
+        for conn in self.connections:
+            conn.close()
+        return 
 
-	def run(self):
-                logging.info("Job %s: is now running"%(self.name))
-                '''
-                Connect
-                '''
-		self._connect()
-                logging.debug("Job %s: all the machine is now correctly connected"%(self.name))
-		"""
-                Execute commands queue on all hosts in the list
-                """
-                for step,queue in enumerate(self.listOFqueue):
-                    print step , queue
-                    # update global queue
-                    try:		
-                            for host, conn in zip(self.machines, self.connections):
-                                    queue.put("break")
-                                    t = threading.Thread(target=self._workFun, args=(host, conn, queue, step))
-                                    t.setDaemon(True)		
-                                    t.start()
-                                    self.threads.append(t)
-                                    logging.info("Job %s: start thread on %s"%(self.name,host))
-                            queue.join()
-                    except KeyboardInterrupt:
-                            logging.info("Job %s: KeyboardInterrupt"%(self.name))
-                            self._quit()
-		self._quit()
-                self.job.done=True
-                logging.info("Job %s: Done!"%(self.name))
+    def run(self):
+        '''
+        Connect
+        '''
+        logging.info("Job %s: is now running"%(self.name))
+        self._connect()
+        logging.debug("Job %s: all the machine is now correctly connected"%(self.name))
+        """
+        Execute commands queue on all hosts in the list
+        """
+        for step,queue in enumerate(self.listOFqueue):
+            print step , queue
+            # update global queue
+            try:        
+                for host, conn in zip(self.machines, self.connections):
+                    queue.put("break")
+                    t = threading.Thread(target=self._workFun, args=(host, conn, queue, step))
+                    t.setDaemon(True)        
+                    t.start()
+                    self.threads.append(t)
+                    logging.info("Job %s: start thread on %s"%(self.name,host))
+                queue.join()
+            except KeyboardInterrupt:
+                logging.info("Job %s: KeyboardInterrupt"%(self.name))
+                self._quit()
+        self._quit()
+        self.job.done=True
+        logging.info("Job %s: Done!"%(self.name))
 
 class WorkerManager():
-	'''
-	Manager for multiple jobs
-	'''
-	def __init__(self):
-		self._jobs = {} # jobID:{"machine":[machinediz1, ..], "program":[programdiz1,...]}
+    '''
+    Manager for multiple jobs
+    '''
+    def __init__(self):
+        self._jobs = {} # jobID:{"machine":[machinediz1, ..], "program":[programdiz1,...]}
 
-	def getJob(self,name):
-		return self._jobs[name]
-	def addJob(self,job):
-		self._jobs[job.name] = job
-		#START-JOB
-	def startJob(self,name):
-		self.test(name)
+    def getJob(self,name):
+        return self._jobs[name]
+    def addJob(self,job):
+        self._jobs[job.name] = job
+        #START-JOB
+    def startJob(self,name):
+        self.test(name)
                 
-	def test(self,name):
-		job=self._jobs[name]
+    def test(self,name):
+        job=self._jobs[name]
                 logging.debug("TEST - JOB %s"%(job.name))
                 logging.debug("TEST - JOB %s"%(job.machines))
                 logging.debug("TEST - JOB %s"%(job.programs))
-		j=WorkerJob(job)
-		j.start()
+        j=WorkerJob(job)
+        j.start()
 
 class Job():
-	'''
-	Job structure (and record??) running in WorkerManager
-	'''
-	def __init__(self,jobID):
-		self.name = jobID
-		self.machines = []
-		self.programs = []
+    '''
+    Job structure (and record??) running in WorkerManager
+    '''
+    def __init__(self,jobID):
+        self.name = jobID
+        self.machines = []
+        self.programs = []
                 self.done = False
                 self.stdout = Queue.Queue()
                 self.stderr = Queue.Queue()
@@ -205,8 +205,8 @@ class Job():
                 self.status = Queue.Queue()
                 self.error = Queue.Queue()
                 self.status_error = False
-		
-		
+        
+        
 
 class Server():
     '''
@@ -217,8 +217,8 @@ class Server():
         # run server
         #except:
         # restart server
-	self.manager = WorkerManager()
-	self.msgHandler = JSON()
+    self.manager = WorkerManager()
+    self.msgHandler = JSON()
         ####
         # Create a server
         logging.info("Start RunnerPyzza Server")
@@ -231,13 +231,13 @@ class Server():
         ####
         
         self.iPP=iProtocol()
-	self.oPP=oProtocol()
+    self.oPP=oProtocol()
         self.ok = self.oPP.interpretate(System("ok"))
         self.fail = self.oPP.interpretate(System("fail"))
         
         
         quit=False
-	while not quit:
+    while not quit:
             logging.info("Server is now waiting for client on port %s"%(port))
             # Attendi la connessione del lanciatore...
             client_socket, address = server_socket.accept()
@@ -248,7 +248,7 @@ class Server():
             client_data = client_socket.recv(1024)
             self.iPP.interpretate(client_data)
             if self.iPP.type=="system":
-		if self.iPP.obj.body == "init":
+        if self.iPP.obj.body == "init":
                     client_socket.send(self.ok)
                     self._initJob(client_socket)
                     
@@ -286,7 +286,7 @@ class Server():
             else:
                 client_socket.send(self.fail)
             
-            client_data = client_socket.recv(1024)		
+            client_data = client_socket.recv(1024)        
             if self.iPP.type=="system":
                 if self.iPP.obj.body == "quit":
                     client_socket.close()
@@ -406,12 +406,12 @@ class Server():
         while 1:
             client_data = client_socket.recv(1024)
             self.iPP.interpretate(client_data)
-            if self.iPP.type == "machine":	
+            if self.iPP.type == "machine":    
                 logging.debug("Machine :%s"%(client_data))
                 job.machines.append(self.iPP.obj)
                 client_socket.send(self.ok)
 
-            elif self.iPP.type == "program":	
+            elif self.iPP.type == "program":    
                 logging.debug("Program :%s"%(client_data))
                 job.programs.append(self.iPP.obj)
                 client_socket.send(self.ok)
@@ -436,7 +436,7 @@ class Server():
         
     
 
-					
+                    
 ################################################################################
 # Methods
 
@@ -444,4 +444,4 @@ class Server():
 # Main
 
 if __name__ == '__main__':
-	pass
+    pass
