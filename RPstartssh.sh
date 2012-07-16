24b8381627738ed6768bdcbbdd8d6b66d327aff9
Marco Galardini <galactus8403@gmail.com>
2012-07-16 18:00:04 +0200

    ssh-agent madness

--- RPstartssh.sh
+++ RPstartssh.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Start the ssh-agent for the user runnerpyzza
-SOCKET=~/.ssh/agent.socket
-ENV=~/.ssh/agent.env
-ssh-agent -a $SOCKET > $ENV
-export SSH_AUTH_SOCK=~/.ssh/agent.socket
+eval $(ssh-agent)
